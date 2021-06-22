741d9aef99cafc507d1245c9d8595400e5c0e95d
Daniel <dpere381@fiu.edu>
2021-06-22 13:02:42 -0400

    added langs

--- js/GitWidget.js
+++ js/GitWidget.js
@@ -1,14 +1,28 @@
 var app = angular.module('gitWidget', []);
-app.controller('git', function($scope, $http) {
-    $http({
-        method: "GET",
-        url: `https://api.github.com/users/dpere122/repos`
-    }).then(function success(response) {
+app.controller('git', function($scope, $http,$q) {
+    // $http({
+    //     method: "GET",
+    //     url: 
+    // }).then(function success(response) {
+    //     $scope.projects = retProjects(response.data);
+    // }, function error(response) {
+    //     $scope.projects = response.statusText;
+    // });
+    $http({method:"GET",url: `https://api.github.com/users/dpere122/repos`}).then(function success(response){
         $scope.projects = retProjects(response.data);
-    }, function error(response) {
-        $scope.projects = response.statusText;
+        console.log($scope.projects[0].languages);
+        $scope.techs = [];
+        for(let i = 0; i< $scope.projects.length -1; i++){
+            $http({method:'GET', url: $scope.projects[i].languages}).then(function success(response){
+                $scope.techs.push(response.data);
+
+            });
+        }
+        console.log($scope.techs);
     });
 });
+
+
 
 function retProjects(data) {
     let projects = [];
