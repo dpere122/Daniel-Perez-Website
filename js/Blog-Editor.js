4b021bd4dc5259a25a71b6561d88af99603c5105
Daniel <dpere381@fiu.edu>
2021-07-23 01:27:01 -0400

    Added edit mode for existing posts, posts are sorted with date posted data

--- js/Blog-Editor.js
+++ js/Blog-Editor.js
@@ -3,12 +3,20 @@ var app = angular.module('blog-editor', []);
 app.controller('create-post', function($scope, $http) {
     $scope.title = null;
     $scope.content = null;
+    $scope.editMode = false;
+    postID = null;
     $scope.loadPosts = function() {
         $http.get("http://localhost:8080/api/posts/getall")
             .then(function(response) {
                 $scope.posts = response.data;
             });
     }
+    $scope.setText = function(post) {
+        postID = post.id;
+        $scope.title = post.title;
+        $scope.content = post.content;
+        $scope.editMode = true;
+    };
     $scope.deletePost = function(id) {
         $http.delete('http://localhost:8080/api/posts/delete/' + id)
             .then(function(response) {
@@ -35,4 +43,22 @@ app.controller('create-post', function($scope, $http) {
         }
 
     };
+    $scope.putData = function(title, content) {
+        var data = {
+            title: title,
+            content: content
+        };
+        if ($scope.editMode === true) {
+            $scope.editMode = false;
+            $http.put('http://localhost:8080/api/posts/edit/' + postID, JSON.stringify(data))
+                .then(function(response) {
+                    $scope.msg = "Post sent successfully!";
+                }, function(response) {
+                    $scope.msg = "Error sending new post!"
+                    $scope.statusval = response.status;
+                    $scope.statustext = response.statusText;
+                    $scope.headers = response.headers();
+                });
+        }
+    }
 });
