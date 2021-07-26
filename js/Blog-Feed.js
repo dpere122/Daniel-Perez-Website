7e59f55cac4464d613e61e9aadb5bd5e5652cde4
Daniel <dpere381@fiu.edu>
2021-07-26 14:34:57 -0400

    Started working on displaying a single post

--- js/Blog-Feed.js
+++ js/Blog-Feed.js
@@ -1,19 +1,30 @@
 var app = angular.module('blog-feed', ['ngSanitize']);
-app.controller('feed-controller',
-    function($scope, $http, $sce) {
-        $http.get("http://localhost:8080/api/posts/getall")
-            .then(function(response) {
-                let convertedPosts = []
-                data = response.data;
-                for (let counter = 0; counter < data.length; counter++) {
-                    nPost = {
-                        title: data[counter].title,
-                        content: $sce.trustAsHtml(data[counter].content)
-                    };
-                    convertedPosts.push(nPost);
-                }
-                $scope.posts = convertedPosts;
-                console.log(convertedPosts);
 
-            });
-    });
+app.factory('postService', function() {
+    let curPost = null;
+    return curPost;
+});
+app.controller('feed-controller', function($scope, $http, $sce, $window, postService) {
+    $scope.curPost = null;
+    $http.get("http://localhost:8080/api/posts/getall")
+        .then(function(response) {
+            let convertedPosts = []
+            data = response.data;
+            for (let counter = 0; counter < data.length; counter++) {
+                nPost = {
+                    title: data[counter].title,
+                    content: $sce.trustAsHtml(data[counter].content)
+                };
+                convertedPosts.push(nPost);
+            }
+            $scope.posts = convertedPosts;
+        });
+    $scope.savePost = function(post) {
+        postService.curPost = post;
+        $window.location.href = '../blog/blog-post.html';
+    };
+});
+app.controller('post-controller', function($scope, postService) {
+    $scope.post = postService.curPost;
+    console.log(post);
+});
